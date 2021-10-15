b6d2ef96dd195d5a1ec8e915dd74578cb0b7900f
Ayaan Mohamed <ayaanestlydontknow@gmail.com>
2021-10-15 15:50:05 -0500

    Complete

--- variables.js
+++ variables.js
@@ -2,16 +2,22 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+var myName = "Ayaan";
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+var faveNum = 3
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+
+var lovesCode = true
+console.log(typeof lovesCode); //test
+
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -22,9 +28,19 @@
 
 //CODE HERE
 
+if (faveNum === 13) {
+  console.log("lucky 13!.");
+}else {
+  console.log("not lucky 13.");
+}
+
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+
+for (let i = 0; i < 5; i++){
+  console.log(faveNum);
+}
