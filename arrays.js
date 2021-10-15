8d65fc96f962add23bda541709c18e090b2d0c00
Ayaan Mohamed <ayaanestlydontknow@gmail.com>
2021-10-15 18:39:29 -0500

    Complete

--- arrays.js
+++ arrays.js
@@ -1,9 +1,7 @@
 //////////////////PROBLEM 1////////////////////
-/* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
 
 var colorCopy = faveColors.slice();
 
@@ -11,41 +9,20 @@ console.log(colorCopy);
 
 
 //////////////////PROBLEM 2////////////////////
-/* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
-//CODE HERE
 colorCopy.push('pink');
 console.log(colorCopy);
 
 
 //////////////////PROBLEM 3////////////////////
-/*
-  Using an array method, create a new array called 'middleNums' from the 
-  numbers array that will capture only the middle numbers (2, 3, 4).
-*/
 
 const numbers = [1, 2, 3, 4, 5]
 
-
-//CODE HERE
 
 const middlenums = numbers.slice(1,4);
 console.log(middlenums)
 
 //////////////////PROBLEM 4////////////////////
-/* 
-  Create a function called 'bigOrSmall' that takes in one parameter, 'arr', 
-  which will be an array of numbers. 
-  Inside of the bigOrSmall function, create a new array called 'answers'. 
-  Then, loop over the passed in arr parameter, and check to see if the number in the
-  array is GREATER than 100. 
-  If it is, push 'big' as a string to the answers array. 
-  If the number is LESS than or EQUAL to 100, push 'small' as a string 
-  to the answers array. 
-  Return the answers array inside of the function.
-*/
-
-// CODE HERE
 
 
 function bigOrSmall(arr) {
@@ -57,4 +34,4 @@ function bigOrSmall(arr) {
   } console.log(answers);
 }
 
-console.log("I gave this one my all. :)")
+console.log("I gave this last one my all.");
