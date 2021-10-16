3ae9413a469ba1fa7154fac37271d1d45b25339f
Ayaan Mohamed <ayaanestlydontknow@gmail.com>
2021-10-15 20:25:39 -0500

    Solid effort, pat on back

---
+++ password.js
@@ -0,0 +1,9 @@
+var chars =10
+
+  reader.question("Hi, Welcome to our site. Enter new password: ", function(password) {
+        if (password >= ){
+        console.log("Your new password is set!");
+    }else{
+        console.log("Your password does not meet requirements. Try again.");
+    }
+}) //Gave this my all
